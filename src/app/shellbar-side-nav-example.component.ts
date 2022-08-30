78787f3f19b895011e40099cdd42d23d5f14b8ca
habibuSAP <habibu.abdullahi@sap.com>
2022-08-30 14:31:09 +0200

    add: menus

--- src/app/shellbar-side-nav-example.component.ts
+++ src/app/shellbar-side-nav-example.component.ts
@@ -1,11 +1,65 @@
 import { Component, ViewEncapsulation } from '@angular/core';
+import { ShellbarMenuItem, ShellbarUser, ShellbarUserMenu } from '@fundamental-ngx/core';
 
 @Component({
-    selector: 'fd-shellbar-side-nav-example',
-    templateUrl: './shellbar-side-nav-example.component.html',
-    styleUrls: ['./shellbar-side-nav-example.component.scss'],
-    encapsulation: ViewEncapsulation.None
+  selector: 'fd-shellbar-side-nav-example',
+  templateUrl: './shellbar-side-nav-example.component.html',
+  styleUrls: ['./shellbar-side-nav-example.component.scss'],
+  encapsulation: ViewEncapsulation.None,
 })
 export class ShellbarSideNavExampleComponent {
-    condensed = false;
+  condensed = false;
+  user: ShellbarUser =
+      {
+        fullName: 'Habibu Abdullahi',
+        colorAccent: 7
+      };
+
+  userMenu: ShellbarUserMenu[] = [
+    {
+      text: 'Settings',
+      callback: this.settingsCallback,
+    },
+    {
+      text: 'Logout',
+      callback: this.logoutCallback,
+    },
+  ];
+
+  settingsCallback(): void {
+    alert('Setting cliked');
+  }
+
+  logoutCallback(): void {
+    alert('Logout clicked');
+  }
+
+  productMenuControl = 'Administration Cockpit';
+
+  productMenuItems: ShellbarMenuItem[] = [
+    {
+      name: 'Administration Cockpit',
+      callback: () => {
+        alert('Admin cockpit clicked');
+      },
+    },
+    {
+      name: 'Product Cockpit',
+      callback: () => {
+        alert('Product clicked');
+      },
+    },
+    {
+      name: 'Adaptive Search',
+      callback: () => {
+        alert('Search clicked');
+      },
+    },
+    {
+      name: 'Extensions',
+      callback: () => {
+        alert('Extensions  clicked');
+      },
+    },
+  ];
 }
